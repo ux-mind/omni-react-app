ff01d2a10c0d471a35dc6a85dc5eff356e61608a
JustShiba <liyalisica9356@gmail.com>
2022-11-06 11:49:02 +0300

    feat: add world map section

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,13 @@
 import './mainPage.scss';
 import MainSection from './MainSection/MainSection.jsx';
 import GraphicSection from './GraphicSection/GraphicSection.jsx';
+import WorldBGSection from './WorldBGSection/WorldBGSection.jsx';
 
 const MainPage = () => (
   <main>
     <MainSection />
     <GraphicSection />
+    <WorldBGSection />
   </main>
 );
 
---
+++ src/pages/MainPage/WorldBGSection/WorldBGSection.jsx
@@ -0,0 +1,9 @@
+import './WorldBGSection.scss';
+
+const WorldBGSection = () => (
+  <div className="map-section">
+    <img className="map-background" src="/src/assets/backgrounds/worldMap.svg" alt="World map" />
+  </div>
+);
+
+export default WorldBGSection;
