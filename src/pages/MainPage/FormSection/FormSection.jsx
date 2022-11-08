d552237fa9f33fc3e07e1a0076c3fa168e82ef31
JustShiba <liyalisica9356@gmail.com>
2022-11-08 14:17:09 +0300

    feat: add behavior on checkbox click

--- src/pages/MainPage/FormSection/FormSection.jsx
+++ src/pages/MainPage/FormSection/FormSection.jsx
@@ -1,4 +1,5 @@
 import './formSection.scss';
+import { useState } from 'react';
 import Logo from '../../../assets/Logo.jsx';
 import Input from '../../../components/Input/Input.jsx';
 import Button from '../../../components/Button/Button.jsx';
@@ -22,35 +23,41 @@ const itemsForDropdown = [
   },
 ];
 
-const FormSection = () => (
-  <div className="form-section">
-    <h2>
-      Do you see a need to disrupt broadcasting?
-      We would love to empower you!
-    </h2>
-    <div className="form-frame">
-      <div className="form-frame-box">
-        <h4>Radio Waves Are Open</h4>
-        <div className="group-inputs">
-          <Input className="margin-r" title="First name" placeholder="First name" />
-          <Input title="Last name" placeholder="Last name" />
+const FormSection = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  return (
+    <div className="form-section">
+      <h2>
+        Do you see a need to disrupt broadcasting?
+        We would love to empower you!
+      </h2>
+      <div className="form-frame">
+        <div className="form-frame-box">
+          <h4>Radio Waves Are Open</h4>
+          <div className="group-inputs">
+            <Input className="margin-r" title="First name" placeholder="First name" />
+            <Input title="Last name" placeholder="Last name" />
+          </div>
+          <Input type="dropdown" title="User type" placeholder="Interest" itemsForDropdown={itemsForDropdown} />
+          <Input title="Email" placeholder="you@.com" />
+          <Input type="text-area" title="Message" height={125} />
+          <div className="checkbox-input">
+            <div className="personal-checkbox" onClick={() => setIsChecked(!isChecked)}>
+              {isChecked && <img src="/src/assets/icons/check.svg" alt="check" />}
+            </div>
+            <p>You agree to our friendly privacy policy.</p>
+          </div>
+          <div className="center">
+            <Button className="square" title="Send" />
+          </div>
         </div>
-        <Input type="dropdown" title="User type" placeholder="Interest" itemsForDropdown={itemsForDropdown} />
-        <Input title="Email" placeholder="you@.com" />
-        <Input type="text-area" title="Message" height={125} />
-        <div className="checkbox-input">
-          <div className="personal-checkbox" />
-          <p>You agree to our friendly privacy policy.</p>
-        </div>
-        <div className="center">
-          <Button className="square" title="Send" />
-        </div>
+        <Logo className="first-logo" size={40} />
+        <Logo className="second-logo" size={30} />
+        <Logo className="third-logo" size={20} />
       </div>
-      <Logo className="first-logo" size={40} />
-      <Logo className="second-logo" size={30} />
-      <Logo className="third-logo" size={20} />
     </div>
-  </div>
-);
+  );
+};
 
 export default FormSection;
