5def0c29ef6bba946eb64a82d0905390d1f72a64
JustShiba <liyalisica9356@gmail.com>
2022-11-09 11:18:36 +0300

    fix: error in phone only component

--- src/pages/MainPage/PhoneOnlySection/PhoneOnlySection.jsx
+++ src/pages/MainPage/PhoneOnlySection/PhoneOnlySection.jsx
@@ -5,15 +5,15 @@ const PhoneOnlySection = () => (
   <div className="phoneOnly-section">
     <div className="phoneOnly-section-background">
       <div className="phoneOnly-section-container">
-        <Logo size="20" className="logo left-first" />
-        <Logo size="30" className="logo left-second" />
-        <Logo size="40" className="logo left-third" />
-        <Logo size="56" className="logo left-fourth" />
+        <Logo size={20} className="logo left-first" />
+        <Logo size={30} className="logo left-second" />
+        <Logo size={40} className="logo left-third" />
+        <Logo size={56} className="logo left-fourth" />
         <img src="/src/assets/phones/phone_5.png" alt="phone 5" />
-        <Logo size="20" className="logo right-first" />
-        <Logo size="30" className="logo right-second" />
-        <Logo size="40" className="logo right-third" />
-        <Logo size="56" className="logo right-fourth" />
+        <Logo size={20} className="logo right-first" />
+        <Logo size={30} className="logo right-second" />
+        <Logo size={40} className="logo right-third" />
+        <Logo size={56} className="logo right-fourth" />
       </div>
     </div>
   </div>
