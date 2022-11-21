1b88ad58263993ecd35f6eb1adb2a946f33bee98
JustShiba <liyalisica9356@gmail.com>
2022-11-21 12:53:13 +0300

    feat: remove React.lazy

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,39 +1,47 @@
 import './mainPage.scss';
-import { lazy, Suspense } from 'react';
+import MainSection from './MainSection/MainSection.jsx';
+import GraphicSection from './GraphicSection/GraphicSection.jsx';
+import WorldBGSection from './WorldBGSection/WorldBGSection.jsx';
+import FacilitiesSection from './FacilitiesSection/FacilitiesSection.jsx';
+import ChangingLifeSection from './ChangingLifeSection/ChangingLifeSection.jsx';
+import FutureSection from './FutureSection/FutureSection.jsx';
+import ControlSection from './ControlSection/ControlSection.jsx';
+import ExperienceSection from './ExperienceSection/ExperienceSection.jsx';
+import BubblehouseSection from './BubblehouseSection/BubblehouseSection.jsx';
+import FormSection from './FormSection/FormSection.jsx';
+import PhoneOnlySection from './PhoneOnlySection/PhoneOnlySection.jsx';
+import DiversitySection from './DiversitySection/DiversitySection.jsx';
+import MemoriesSection from './MemoriesSection/MemoriesSection.jsx';
 
-const MainSection = lazy(() => import('./MainSection/MainSection.jsx'));
-const GraphicSection = lazy(() => import('./GraphicSection/GraphicSection.jsx'));
-const WorldBGSection = lazy(() => import('./WorldBGSection/WorldBGSection.jsx'));
-const FacilitiesSection = lazy(() => import('./FacilitiesSection/FacilitiesSection.jsx'));
-const ChangingLifeSection = lazy(() => import('./ChangingLifeSection/ChangingLifeSection.jsx'));
-const FutureSection = lazy(() => import('./FutureSection/FutureSection.jsx'));
-const MemoriesSection = lazy(() => import('./MemoriesSection/MemoriesSection.jsx'));
-const ControlSection = lazy(() => import('./ControlSection/ControlSection.jsx'));
-const ExperienceSection = lazy(() => import('./ExperienceSection/ExperienceSection.jsx'));
-const BubblehouseSection = lazy(() => import('./BubblehouseSection/BubblehouseSection.jsx'));
-const FormSection = lazy(() => import('./FormSection/FormSection.jsx'));
-const PhoneOnlySection = lazy(() => import('./PhoneOnlySection/PhoneOnlySection.jsx'));
-const DiversitySection = lazy(() => import('./DiversitySection/DiversitySection.jsx'));
+// const MainSection = lazy(() => import('./MainSection/MainSection.jsx'));
+// const GraphicSection = lazy(() => import('./GraphicSection/GraphicSection.jsx'));
+// const WorldBGSection = lazy(() => import('./WorldBGSection/WorldBGSection.jsx'));
+// const FacilitiesSection = lazy(() => import('./FacilitiesSection/FacilitiesSection.jsx'));
+// const ChangingLifeSection = lazy(() => import('./ChangingLifeSection/ChangingLifeSection.jsx'));
+// const FutureSection = lazy(() => import('./FutureSection/FutureSection.jsx'));
+// const MemoriesSection = lazy(() => import('./MemoriesSection/MemoriesSection.jsx'));
+// const ControlSection = lazy(() => import('./ControlSection/ControlSection.jsx'));
+// const ExperienceSection = lazy(() => import('./ExperienceSection/ExperienceSection.jsx'));
+// const BubblehouseSection = lazy(() => import('./BubblehouseSection/BubblehouseSection.jsx'));
+// const FormSection = lazy(() => import('./FormSection/FormSection.jsx'));
+// const PhoneOnlySection = lazy(() => import('./PhoneOnlySection/PhoneOnlySection.jsx'));
+// const DiversitySection = lazy(() => import('./DiversitySection/DiversitySection.jsx'));
 
 const MainPage = () => (
   <main>
-    <Suspense>
-      {/* animated */}
-      <MainSection />
-      <GraphicSection />
-      <WorldBGSection />
-      <FacilitiesSection />
-      <ChangingLifeSection />
-      <FutureSection />
-      {/* animated */}
-      <MemoriesSection />
-      <ControlSection />
-      <ExperienceSection />
-      <BubblehouseSection />
-      <FormSection />
-      <PhoneOnlySection />
-      <DiversitySection />
-    </Suspense>
+    <MainSection />
+    <GraphicSection />
+    <WorldBGSection />
+    <FacilitiesSection />
+    <ChangingLifeSection />
+    <FutureSection />
+    <MemoriesSection />
+    <ControlSection />
+    <ExperienceSection />
+    <BubblehouseSection />
+    <FormSection />
+    <PhoneOnlySection />
+    <DiversitySection />
   </main>
 );
 
